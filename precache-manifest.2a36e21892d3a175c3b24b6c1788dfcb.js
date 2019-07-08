e1a3de066e4737e8ec9ea47b490ea5282efa5bbc
Andreev Denis <andreev@Mac-mini.local>
2019-07-08 14:49:23 +0700

    Updates

---
+++ precache-manifest.2a36e21892d3a175c3b24b6c1788dfcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/group-freind/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "025ed306b67615a185dc",
+    "url": "/group-freind/static/js/runtime~main.6bcdf87f.js"
+  },
+  {
+    "revision": "2d37aa8bb48ba872039c",
+    "url": "/group-freind/static/js/main.33658776.chunk.js"
+  },
+  {
+    "revision": "e66473865b978f778231",
+    "url": "/group-freind/static/js/2.ad4c9ac3.chunk.js"
+  },
+  {
+    "revision": "2d37aa8bb48ba872039c",
+    "url": "/group-freind/static/css/main.0619d5b9.chunk.css"
+  },
+  {
+    "revision": "e66473865b978f778231",
+    "url": "/group-freind/static/css/2.23087338.chunk.css"
+  },
+  {
+    "revision": "64e6f486f415ee534411f5d34478c5db",
+    "url": "/group-freind/index.html"
+  }
+];
